ac4830da31eda54d5bcceb6b5e917bf7a09d5ea0
STROMPYNO <104005915+STROMPYNO@users.noreply.github.com>
2023-05-20 16:12:41 +0200

    Add files via upload

---
+++ RotateGun.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour
+{
+    public GrapplinGun grappling;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+
+    void Update()
+    {
+        if (!grappling.IsGrappling())
+        {
+            desiredRotation = transform.parent.rotation;
+        }
+        else
+        {
+            desiredRotation = Quaternion.LookRotation(grappling.GetGrapplePoint() - transform.position);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * rotationSpeed);
+    }
+
+}
